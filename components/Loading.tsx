56a4c35638b2c1ec9af8d48ee81492c667003be2
evandrosaturnino <evandro.saturnino@gmail.com>
2023-06-30 18:29:15 -0300

    feat: new loading image

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,10 +1,11 @@
+import Image from "next/image";
+
 const Loading = (): JSX.Element => {
   return (
-    <div className="flex items-center justify-center loading-spinner">
-      <div className="spinner"></div>
-      <p className="loading-text ml-4 text-center">
-        Loading
-      </p>
+    <div className="flex items-center justify-center h-screen">
+      <div className="relative w-48 h-48 animate-pulse">
+        <Image src="/icons/loading.svg" alt="thresholdusd loading" fill={true} sizes="(min-width: 1rem) 24vw" />
+      </div>
     </div>
   );
 };
